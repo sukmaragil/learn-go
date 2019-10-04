66e88ac260f6a4b0d55dc12e2a21d794897deb59
sukmaragilp <amaliya.sukma@icehousecorp.com>
2019-10-04 14:10:09 +0700

    refactor base Hello world

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello print hello
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
